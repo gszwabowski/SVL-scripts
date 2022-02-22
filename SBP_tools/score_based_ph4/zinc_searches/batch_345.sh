bbd90971ff0cc1a7663e10923cc1fd7babe4de35
Gregory Szwabowski <gszwabowski@gmail.com>
2022-02-22 15:40:15 -0600

    removed old directories

--- SBP_tools/score_based_ph4/zinc_searches/batch_345.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/csh
-#SBATCH --ntasks=4
-#SBATCH --partition=computeq
-#SBATCH --job-name=_zinc_search
-
-
-/public/apps/moe/moe2018/bin/moebatch -load zinc_search_subset_345.svl \
--exec "zinc_search_subset []"
-
